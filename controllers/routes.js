d721c9b82ff00df688aecdf7546ba32bb4168927
Farai Matangira <bm5w40i@yahoo.com>
2020-02-16 21:14:30 -0600

    insert comments in the routes.js file

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,8 +1,11 @@
+// Import models from the models folder to use when querying database using sequelize
 const db = require("../models");
 
+// Export routes below to server.js file
 module.exports = function (app) {
     
     app.get('/api/movies',function(req,res){
+        // Search for all films, then send the json data to client
         db.film.findAll().then(data => res.json(data));
     })
 }
